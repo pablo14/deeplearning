04d73f3760d546e27ae25652fc924ccf7e1d5f4d
pablo14 <pcasas.biz@gmail.com>
2016-04-12 14:43:42 -0300

    deleted bad graphs

--- som/som.R
+++ som/som.R
@@ -1,4 +1,3 @@
-#install.packages("kohonen")
 library(kohonen)
 data(wines)
 set.seed(7)
@@ -9,10 +8,18 @@ Xtest <- scale(wines[-training, ],
                center = attr(Xtraining, "scaled:center"),
                scale = attr(Xtraining, "scaled:scale"))
  
+set.seed(099999)
 ## SOM => Unsupervised 
 som.wines = som(Xtraining, grid = somgrid(5, 5, "hexagonal"))
+
+plot(som.wines, type="counts", palette.name=coolBlueHotRed)
+
+## cases per neuron
+library(dplyr)
+t=group_by(data.frame(v=som.wines$unit.classif),v) %>% summarize(q=n()) %>% arrange(-q)
+
  
-som.prediction <- predict(som.wines, newdata = Xtest,
+som.prediction <- predict(sm.wines, newdata = Xtest,
                           trainX = Xtraining,
                           trainY = factor(wine.classes[training]))
 
@@ -22,11 +29,15 @@ plot(som.wines)
 
 summary(som.wines)
 
-#####################
-plot(kohmap, type="changes")
-plot(kohmap, type="codes", main = c("Codes X", "Codes Y"))
-plot(kohmap, type="counts")
+## The lower the better since the neuron is representing quite well the input case
+plot(som.wines, type = "quality", main="Node Quality/Distance", palette.name=coolBlueHotRed)
 
+plot(som.wines, type="changes")
+plot(som.wines, type="codes", main = c("Codes X", "Codes Y"))
+plot(som.wines, type="counts")
+
+
+#####################
 ## palette suggested by Leo Lopes
 coolBlueHotRed <- function(n, alpha = 1) {
 rainbow(n, end=4/6, alpha=alpha)[n:1]
